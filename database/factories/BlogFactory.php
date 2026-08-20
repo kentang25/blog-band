f733bc81788c15cbc6d529cecc42e47612c48c77
REGI RIDHO <regiyanuar123@gmail.com>
2026-08-20 15:24:32 +0700

    masih membuat pondasi website

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Blog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Blog>
+ */
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'slug'  => $this->faker->slug(),
+            'content' => $this->faker->paragraph(),
+            'gambar' => $this->faker->imageUrl(),
+        ];
+    }
+}
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config('app.name', 'laravel') }}</title>
+
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    </head>
+    <body>
+        {{ $slot }}
+    </body>
+</html>
